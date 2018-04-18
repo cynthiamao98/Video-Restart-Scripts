8cf43a69acc97fe94a073f8f123f259046805b3b
cynthiamao98 <32347565+cynthiamao98@users.noreply.github.com>
2018-04-18 18:00:58 -0400

    get rid of vuforia copyright
    cuz i wrote the script

--- VideoController.cs
+++ VideoController.cs
@@ -1,9 +1,3 @@
-﻿/*===============================================================================
-Copyright (c) 2017 PTC Inc. All Rights Reserved.
-
-Vuforia is a trademark of PTC Inc., registered in the United States and other 
-countries.
-===============================================================================*/
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
