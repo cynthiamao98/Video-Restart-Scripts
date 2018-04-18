75a6d04e75efbe995b6e7d7bcd2e9756307ce0b7
cynthiamao98 <32347565+cynthiamao98@users.noreply.github.com>
2018-04-18 18:00:18 -0400

    Update RestartVideoTrackableEventHandler.cs

--- RestartVideoTrackableEventHandler.cs
+++ RestartVideoTrackableEventHandler.cs
@@ -1,9 +1,3 @@
-/*===============================================================================
-Copyright (c) 2017 PTC Inc. All Rights Reserved.
- 
-Vuforia is a trademark of PTC Inc., registered in the United States and other
-countries.
-===============================================================================*/
 
 public class RestartVideoTrackableEventHandler : DefaultTrackableEventHandler
 {
